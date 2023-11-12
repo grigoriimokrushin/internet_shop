5a1d41b85a2b74afe5562de6879bf4a1c454aa95
Grigory Mokrushin <wedass@mail.ru>
2023-11-12 18:46:08 +0700

    HW дополнительное задание

--- catalog/catalog/views.py
+++ catalog/catalog/views.py
@@ -7,4 +7,10 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(name, phone, message)
+
     return render(request, 'contacts.html')
